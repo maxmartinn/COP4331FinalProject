6c0c12ffbd8f3b8e21562aa2ba3afb6d5db0d252
buildmine10 <collin_stebbins@yahoo.com>
2024-11-01 18:26:32 -0400

    Made the lexer

--- src/main/java/oop/project/library/Main.java
+++ src/main/java/oop/project/library/Main.java
@@ -1,30 +1,74 @@
 package oop.project.library;
 
+import oop.project.library.lexer.LexerArgument;
 import oop.project.library.scenarios.Result;
 import oop.project.library.scenarios.Scenarios;
 
-import java.util.Map;
-import java.util.Scanner;
+import java.util.*;
 
 public class Main {
 
     public static void main(String[] args) {
-        var scanner = new Scanner(System.in);
-        while (true) {
-            var input = scanner.nextLine();
-            if (input.equals("exit")) {
-                break;
-            }
-            try {
-                var result = Scenarios.parse(input);
-                switch (result) {
-                    case Result.Success<Map<String, Object>> success -> System.out.println(success.value());
-                    case Result.Failure<Map<String, Object>> failure -> System.out.println("Error: " + failure.error());
-                }
-            } catch (Exception e) {
-                System.out.println("Unexpected exception: " + e.getClass().getName() + ", " + e.getMessage());
-            }
+        LexerArgument hi = new LexerArgument(LexerArgument.Mode.PRESENCE, "-f", "--flag");
+        LexerArgument hi2 = new LexerArgument(LexerArgument.Mode.MULTI_PARAMETER, "-d", "--deets");
+
+        List<String> inputs = Arrays.asList("-ffff", "Howdy", "-f", "-d", "These are the deets", "--deets", "Ye-haw");
+
+        //LexerArgument hi2 = new LexerArgument(LexerArgument.Mode.SINGLE_PARAMETER, "-d", "--deets");
+        //List<String> inputs = Arrays.asList("-ffff", "Howdy", "-f", "-d", "These are the deets", "--deets", "Ye-haw");//Should crash because two values were given to a one parameter argument
+
+
+        inputs = hi.extract(inputs);
+
+        System.out.println("Remaining Inputs to Lexer");
+        for (var remaining : inputs) {
+            System.out.println(remaining);
         }
+
+
+        System.out.println();
+        System.out.println("Extracted result");
+        System.out.println("Count: " + hi.count);
+
+        for (var bye : hi.strings) {
+            System.out.println(bye);
+        }
+
+        System.out.println();
+
+
+        inputs = hi2.extract(inputs);
+
+        System.out.println("Remaining Inputs to Lexer");
+        for(var remaining : inputs){
+            System.out.println(remaining);
+        }
+
+
+        System.out.println();
+        System.out.println("Extracted result");
+        System.out.println("Count: " + hi2.count);
+
+        for(var bye : hi2.strings){
+            System.out.println(bye);
+        }
+
+        //var scanner = new Scanner(System.in);
+        //while (true) {
+        //    var input = scanner.nextLine();
+        //    if (input.equals("exit")) {
+        //        break;
+        //    }
+        //    try {
+        //        var result = Scenarios.parse(input);
+        //        switch (result) {
+        //            case Result.Success<Map<String, Object>> success -> System.out.println(success.value());
+        //            case Result.Failure<Map<String, Object>> failure -> System.out.println("Error: " + failure.error());
+        //        }
+        //    } catch (Exception e) {
+        //        System.out.println("Unexpected exception: " + e.getClass().getName() + ", " + e.getMessage());
+        //    }
+        //}
     }
 
 }
---
+++ src/main/java/oop/project/library/lexer/Lexer.java
@@ -0,0 +1,24 @@
+package oop.project.library.lexer;
+
+import java.util.AbstractMap;
+import java.util.List;
+
+//This class takes the command line argument strings, and reorganizes them into lists of string that correspond to arguments
+public class Lexer {
+    public AbstractMap<String, List<String>> arguments;//Maps arguments names to lists of strings
+    //I think we should use python's rules for positional and keyword arguments
+
+    //Keyword arguments can break up positional arguments
+    //Only the last positional argument can have variable length
+    //Keyword arguments may not be of variable length
+
+    //We should also have presence flags
+    //We should also have counted flags
+
+    //We should have mutually exclusive flags
+    //We should have mutually inclusive flags
+
+
+
+
+}
